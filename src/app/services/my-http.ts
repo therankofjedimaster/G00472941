512e501ecea809dd35068ae7c0117cfad5eac3f3
therankofjedimaster <98640726+therankofjedimaster@users.noreply.github.com>
2025-12-06 11:50:09 +0000

    Updated HTTP Service
    Updated HTTP service from the code copied from an in-class exercise to something that should work for this assignment. implemented fetching recipes based on ingredients, and based on ID.

--- src/app/services/my-http.ts
+++ src/app/services/my-http.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; // Angular's HttpClient
 import { Observable } from 'rxjs'; // Observable for async operations
-
+import { HttpParams } from '@angular/common/http';
 @Injectable({
   providedIn: 'root',
 })
 export class MyHttp {
-   constructor(private http: HttpClient) {} 
-    private apiUrl = 'https://api.spoonacular.com/recipes/complexSearch';
+  private baseUrl = 'https://api.spoonacular.com/recipes/complexSearch';
   private apiKey = '70759a4f7911402abcc53d3c51d3b759';
-  public getAllRecipes(): Observable<any> {
-    const url = `${this.apiUrl}/all?access_key=${this.apiKey}`;
 
-    // HttpClient's .get() returns an Observable, which is the standard way to handle async data in Angular.
-    return this.http.get(url);
+   constructor(private http: HttpClient) {} 
+    //Fetching recipes based on ingredients
+  public getAllRecipes(ingredients: string): Observable<any> {
+    let params = new HttpParams()
+  .set('apiKey', this.apiKey)
+  .set('query', ingredients);
+return this.http.get(this.baseUrl, { params: params });
+}
+//Fetching recipes details by ID
+public getRecipeDetails(id: number): Observable<any> {
+const detailUrl = `${this.baseUrl}/${id}/information`; 
+let params = new HttpParams()
+.set('apiKey', this.apiKey); 
+return this.http.get(detailUrl, { params: params });
 }
 }
+
