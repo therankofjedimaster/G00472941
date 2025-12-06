7d6ba9bec58c5f33a854fa247db067f63f5668dc
therankofjedimaster <98640726+therankofjedimaster@users.noreply.github.com>
2025-12-06 10:10:55 +0000

    Added services
    Added my-http and data services.
    Imported storage module.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {IonicStorageModule} from '@ionic/storage-angular';
+
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    importProvidersFrom(IonicStorageModule.forRoot()),
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
