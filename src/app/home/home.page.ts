cf9617351088f955dff8122a9b6073bac6a1221b
therankofjedimaster <98640726+therankofjedimaster@users.noreply.github.com>
2025-12-09 18:11:20 +0000

    Fixed the icons visibility
    I have fixed the icons not showing in the page. They show now correctly.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButtons, IonButton, IonLabel, IonInput, IonItem, IonCardContent, IonCard, IonCardHeader, IonCardTitle } from '@ionic/angular/standalone';
-import { heart, settings } from 'ionicons/icons';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButtons, IonButton, IonLabel, IonInput, IonItem, IonCardContent, IonCard, IonCardHeader, IonCardTitle,} from '@ionic/angular/standalone';
 import { MyHttp } from 'src/app/services/my-http';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {addIcons} from 'ionicons';
+import {heart, settings} from 'ionicons/icons';
+
+addIcons({
+  'heart': heart,
+  'settings': settings
+});
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonCardTitle, IonCardHeader, IonCard, IonCardContent, IonItem, IonInput, IonLabel, IonButton, IonButtons, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent, RouterLink, CommonModule, FormsModule],
+  imports: [IonCardTitle, IonCardHeader, IonCard, IonCardContent, IonItem, IonInput, IonLabel, IonButton, IonButtons, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent, RouterLink, CommonModule, FormsModule,],
 })
 export class HomePage {
-  public heartIcon = heart;
-  public settingsIcon = settings;
   public recipes: any[] = [];
   public ingredients: string = '';
   constructor(private httpService: MyHttp) {
